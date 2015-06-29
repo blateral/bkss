0de38388c6c8751c17e91c539a5ac3e876e2b8be
Fabian Trinler <fabian.trinler@blateral.com>
2015-06-29 12:36:01 +0200

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,174 @@
+var browserSync = require("browser-sync");
+
+module.exports = function(grunt) {
+
+    // Load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    var settings = grunt.file.readJSON('settings.json');
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+        settings: grunt.file.readJSON('settings.json'),
+        banner: [
+            '/*!',
+            '<%= pkg.name %>',
+            '@version <%= pkg.version %>',
+            '@date <%= grunt.template.today("yyyy-mm-dd, HH:MM") %>',
+            '*/'
+        ].join("\n"),
+
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            main: {
+                files: {
+                    '<%= settings.css.main.dist %>': '<%= settings.css.main.src %>',
+                }
+            }
+        },
+
+        autoprefixer: {
+            all: {
+                expand: true,
+                flatten: true,
+                src: 'dist/css/*.css',
+                dest: 'dist/css'
+            }
+        },
+
+        cssmin: {
+            css: {
+                options: {
+                    banner: '<%= banner %>'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dist/css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        jshint: {
+            files: [
+                'Gruntfile.js',
+                '<%= settings.js.modules.src %>',
+                '<%= settings.js.main.src %>'
+            ]
+        },
+
+        uglify: {
+            js: {
+                options: {
+                    banner: "<%= banner %>\n"
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/js',
+                    src: ['*.js', '!*.min.js'],
+                    dest: 'dist/js',
+                    ext: '.min.js'
+                }]
+            }
+        },
+
+        copy: {
+            webroot: {
+                expand: true,
+                cwd: 'src/html',
+                src: '**',
+                dest: 'dist/template',
+                dot: true
+            }
+        },
+
+        clean: {
+            dist: 'dist',
+            build: [
+                'dist/tmp',
+                'dist/css/*.map',
+                'dist/css/*.css',
+                '!dist/css/*.min.css',
+                '!dist/css/*.fonts.css',
+                'dist/js/*.js',
+                '!dist/js/*.min.js',
+                '!dist/**/*.custom.*'
+            ]
+        },
+
+        browserify: {
+            dist: {
+                files: {
+                    '<%= settings.js.all.dist %>': ['<%= settings.js.modules.src %>', '<%= settings.js.main.src %>'],
+                },
+                options: {}
+            }
+        },
+
+        kss: {
+            options: {
+                template: 'dist/template'
+            },
+            all: {
+                files: {
+                    'dist/docs': ['src/css/fixtures']
+                }
+            }
+        },
+
+        watch: {
+
+            options: {
+                spawn: false // Very important, don't miss this
+            },
+
+            js: {
+                files: '<%= jshint.files %>',
+                tasks: ['jshint', 'browserify', 'bs-inject-js']
+            },
+
+            scss: {
+                files: '<%= settings.css.scss.src %>',
+                tasks: ['sass', 'autoprefixer', 'kss', 'bs-inject-css']
+            },
+
+            html: {
+                files: 'src/html/**',
+                tasks: ['copy', 'kss', 'bs-inject-html']
+            }
+        }
+
+    });
+
+    grunt.registerTask('bs-init', function () {
+        var done = this.async();
+        browserSync({
+            port: 8000,
+            server: './dist'
+        }, function (err, bs) {
+            done();
+        });
+    });
+
+    grunt.registerTask('bs-inject-css', function () {
+        browserSync.reload(settings.css.main.dist);
+    });
+
+    grunt.registerTask('bs-inject-js', function () {
+        console.log(settings.js.all.dist);
+        browserSync.reload(settings.js.all.dist);
+    });
+
+    grunt.registerTask('bs-inject-html', function () {
+        browserSync.reload();
+    });
+
+    grunt.registerTask('build', ['clean', 'jshint', 'browserify', 'uglify', 'sass', 'autoprefixer', 'cssmin', 'copy', 'clean:build', 'kss']);
+    grunt.registerTask('compile', ['browserify', 'sass', 'autoprefixer', 'copy', 'kss']);
+    grunt.registerTask('default', ['compile' ,'bs-init', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,24 @@
+# b.oilerplate
+Yet another Node.js and GruntJS boilerplate, which features cool tasks like
+
+* compiling your SASS/SCSS through grunt-sass (yeah, the libsass thingy)
+* autoprefixes your CSS
+* hinting, concat and uglifying your javascript
+* building html files with assemble.io and handlebars template
+* you name it
+
+This boilerplate is our personal project kick-off tool and we use it very often. It's only taylored to fit *our* needs. Feel free to use it, but beware, there might be better ones out there...
+
+<3 b.lateral
+
+## Getting started
+This boilerplate makes heavy use of node.js, npm and Grunt. So make sure, these tools and their dependencies are installed before trying to build anything.
+
+1. Run `npm install` from your project root directory
+2. Run `bower install` from your project root directory
+
+## Developing
+1. Run `grunt`
+
+## Building
+1. Run `grunt build`
